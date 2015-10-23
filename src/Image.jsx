89adb37e1675de7861c3fbe4cf62f9840f1efdcb
Hassan Aly <surel@hasssan.com>
2015-10-23 13:59:40 +0700

    Add starGenerator

--- src/Image.jsx
+++ src/Image.jsx
@@ -4,13 +4,23 @@ const Image = React.createClass({
   render() {
     const url = ['https://i.imgur.com/', this.props.data.id, 'b.jpg'].join('');
 
+    const starGenerator = () => {
+      const level = this.props.data.level;
+      const id = this.props.data.id;
+      const star = [];
+
+      for (let i=0; i < level; i++) {
+        star.push(<img src="/star.png" className="star" key={id+i} />)
+      }
+
+      return star;
+    }
+
     return (
       <div className="col s12 m2 item-container">
         <img src={url} className="circle image-item" />
         <div className="star-container center">
-          <img src="/star.png" className="star" />
-          <img src="/star.png" className="star" />
-          <img src="/star.png" className="star" />
+          {starGenerator()}
         </div>
       </div>
     )
